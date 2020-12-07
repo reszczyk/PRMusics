779d80815763ae1e249fdaf9e6c1aeaf8f88f3b5
PR <patrykreszczynski@gmail.com>
2020-12-07 17:43:18 +0100

    Added Music Service

--- app/src/main/java/com/prcoding/prmusics/other/Constants.kt
+++ app/src/main/java/com/prcoding/prmusics/other/Constants.kt
@@ -2,4 +2,9 @@ package com.prcoding.prmusics.other
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+
+    const val MEDIA_ROOT_ID = "root_id"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
