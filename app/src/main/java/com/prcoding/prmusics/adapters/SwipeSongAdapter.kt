63ff95a909a0a9fb9cc2e91120209b97feabb913
PR <patrykreszczynski@gmail.com>
2020-12-08 10:11:05 +0100

    Added Adaptors

---
+++ app/src/main/java/com/prcoding/prmusics/adapters/SwipeSongAdapter.kt
@@ -0,0 +1,24 @@
+package com.prcoding.prmusics.adapters
+
+import androidx.recyclerview.widget.AsyncListDiffer
+import com.prcoding.prmusics.R
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class SwipeSongAdapter : BaseSongAdapter(R.layout.list_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            val text = "${song.title} - ${song.subtitle}"
+            tvPrimary.text = text
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+}
